9883bd9dc62ff24ad81e82d5459bf59c79c2432e
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-04-12 17:55:46 -0500

    Customer

--- src/main/java/com/lambdaschool/orders/models/Customer.java
+++ src/main/java/com/lambdaschool/orders/models/Customer.java
@@ -1,11 +1,48 @@
 package com.lambdaschool.orders.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "customers") //Name of table. It will always be underlined in red
 public class Customer
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long custcode;
+
+    //Fields ----------------------------------------------------------------
+    @Column(nullable = false)
+    private String custname;
+    private String custcity;
+    private String workinarea;
+    private String custcountry;
+    private String grade;
+    private double openingamt;
+    private double reveiveamt;
+    private double paymentamt;
+    private double outstandingamt;
+    private String phone;
+
+    //Join Tables -------------------------------------------------------------
+        //Many to one relationship with agent table. Many customers to one agent.
+    @ManyToOne
+    @JoinColumn(name = "agentcode",
+        nullable = false)
+    @JsonIgnoreProperties(value = "customers",
+        allowSetters = true)
+    private Agent agent;
+
+        //One to Many relationship. One customer to many orders.
+    @OneToMany(mappedBy = "customer",
+        cascade = CascadeType.ALL,
+    orphanRemoval = true)
+    @JsonIgnoreProperties(value = "customer",
+        allowSetters = true)
+    private List<Order> orders = new ArrayList<>();
+
 
 }
